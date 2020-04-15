64dccddadee267d9a3cb1525556e23abe8168e8d
wsy <759387240@qq.com>
2020-04-15 16:24:12 +0800

    model struct json

--- src/structs/fish.go
+++ src/structs/fish.go
@@ -2,14 +2,14 @@ package structs
 
 
 type Fish struct{
-	ID int32 `gorm:"type:int;PRIMARY KEY;AUTO INCREMENT;NOT NULL"`
-	Name string `gorm:"type:varchar(40);NOT NULL"`
-	Alias string `gorm:"type:varchar(40);DEFAULT NULL"` 
-	Max int32 `gorm:"type:int;DEFAULT NULL"`
-	Min int32 `gorm:"type:int;DEFAULT NULL"`
-	Unit int32 `gorm:"type:int;DEFAULT NULL"`
-	Style int32 `gorm:"type:int;DEFAULT NULL"`
-	Probability int32 `gorm:"type:int;DEFAULT NULL"`
-	Position string `gorm:"type:varchar(20);DEFAULT NULL"` 
-	Description string `gorm:"type:varchar(255);DEFAULT NULL"` 
+	ID int32 `gorm:"type:int;PRIMARY KEY;AUTO INCREMENT;NOT NULL" json:"id"`
+	Name string `gorm:"type:varchar(40);NOT NULL" json:"name"`
+	Alias string `gorm:"type:varchar(40);DEFAULT NULL" json:"alias"` 
+	Max int32 `gorm:"type:int;DEFAULT NULL" json:"max"`
+	Min int32 `gorm:"type:int;DEFAULT NULL" json:"min"`
+	Unit int32 `gorm:"type:int;DEFAULT NULL" json:"unit"`
+	Style int32 `gorm:"type:int;DEFAULT NULL" json:"style"`
+	Probability int32 `gorm:"type:int;DEFAULT NULL" json:"probability"`
+	Position string `gorm:"type:varchar(20);DEFAULT NULL" json:"position"` 
+	Description string `gorm:"type:varchar(255);DEFAULT NULL" json:"description"` 
 }
